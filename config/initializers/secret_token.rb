a3e50b2c0d9134446a466dcdea0a5100278403ef
jponcy <poncy.jonathan@gmail.com>
2013-04-29 19:29:59 +0200

    Fix #1 + init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amhiows::Application.config.secret_token = '4b1174c16318a9dcdc5879420fdd4fca6bb5d19b4929c5106132102dada0bd211fe75c81b1818b8f6ecd2805c3c92347c8643a271d0f00bf6f3a841992915dc2'
