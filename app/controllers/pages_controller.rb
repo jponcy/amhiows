ba5e8aa477c62284141b07de58fd438311e634d1
jponcy <poncy.jonathan@gmail.com>
2013-04-29 20:13:06 +0200

    Fix #3 - add statics pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home
+    @title='Accueil'
+  end
+
+  def contact
+    @title='Contact'
+  end
+  
+  def about
+    @title='A propos'
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def title
+    shortTitle='amhiows'
+    if @title.nil?
+      shortTitle
+    else
+      shortTitle + ' | ' + @title
+    end
+  end
 end
